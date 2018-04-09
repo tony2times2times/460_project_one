5059cb89bd02d69010ca80b583df9ad9254aec40
Tony Peraza <tony.peraza86@gmail.com>
2018-04-09 14:21:10 -0500

    started cmd argument parsing

--- src/Client.java
+++ src/Client.java
@@ -5,16 +5,33 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.logging.Logger;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
 /**
  * The Class Client recieves datagrams and assembles a file from those datagrams
  */
 public class Client {
+	static Options options = new Options();
+	static CommandLineParser parser = new DefaultParser();
+	static HelpFormatter formatter = new HelpFormatter();
+	static CommandLine cmd = null;	
+	private static int sizeOfWindow;	
+	private static final int DEFAULT_WINDOW = 1;
+	private static double datagramPercentage = 0.25;
+	private static InetAddress serverAddress;
+	private static int serverPort = 3000;
 
 	/** The running. */
 	private static boolean running = true;
 
 	/** The file path. */
-	private static String filePath = ".\\out.jpg"; // where the output file is written to
+	private static String filePath = ".\\out.txt"; // where the output file is written to
 
 	/** The port. */
 	private static int port = 3000; // port to transmit on
@@ -73,10 +90,95 @@ public class Client {
 		socket.close();
 	}
 	
+	private static void readArgs(String args[]) {
+		defineCommandLineOptions();
+		parseCommandLineArguments(args);
+		setWindow();
+		setDatagram();
+		setServer();
+		displayHelp();
+	}
+	
+	private static void defineCommandLineOptions() {		
+		// window option
+		Option windowOption = new Option("w", "window", true, "sliding window size");
+		windowOption.setRequired(false);
+		options.addOption(windowOption);
+		// drop option
+		Option dropOption = new Option("d", "datagram", true, "percentage of datagrams to corrupt, delay, or drop");
+		dropOption.setRequired(false);
+		options.addOption(windowOption);		
+		Option helpOption = new Option("h", "help", false, "show options");
+		helpOption.setRequired(false);
+		options.addOption(helpOption);
+	}
+	
+	private static void parseCommandLineArguments(String args[]) {
+		try {
+			// parse options into command line
+			cmd = parser.parse(options, args);
+		} catch (ParseException e) {
+			// print exception and help then exit
+			System.out.println(e.getMessage());
+			formatter.printHelp(Server.class.getSimpleName(), options);
+			System.exit(1);
+		}
+	}
+	
+	private static void setWindow() {
+		if (cmd.hasOption("window")) {
+			// sliding window size specified by user
+			sizeOfWindow = Integer.parseInt(cmd.getOptionValue("window"));
+		} else {
+			// sliding window size set to default
+			sizeOfWindow = DEFAULT_WINDOW;
+		}
+	}
+	
+	private static void displayHelp() {
+		if (cmd.hasOption("help")) {
+			// help asked from the user
+			formatter.printHelp(Client.class.getSimpleName(), options);
+			System.exit(0);
+		} else {
+			// help is not asked
+		}
+	}
+	
+	private static void setDatagram() {
+		if (cmd.hasOption("datagram")) {
+			// datagram percentage specified by user
+			datagramPercentage = Double.parseDouble(cmd.getOptionValue("datagram"));
+		} 
+	}
+	
 	private static void sendAck(int ackno, DatagramSocket socket, InetAddress hostAddress) throws IOException {
 		AckPacket ack = new AckPacket((short) 0, ackno);
 		DatagramPacket out = new DatagramPacket(ack.toBytes(), ack.toBytes().length, hostAddress, port);
 		socket.send(out);
+	}
+	
+	private static void setServer() {
+		if (!cmd.getArgList().isEmpty()) {
+			// receiver address set by user
+			byte[] hostAddress = cmd.getArgList().iterator().next().getBytes();
+			try {
+				serverAddress = InetAddress.getByAddress(hostAddress);
+			} catch (UnknownHostException e) {
+				e.printStackTrace();
+				System.exit(1);
+			}
+			// receiver port set by user
+			serverPort = Integer.parseInt(cmd.getArgList().iterator().next());
+		} else {
+			// receiver port set to default
+			try {
+				serverAddress = InetAddress.getByName("localhost");
+			} catch (UnknownHostException e) {
+				e.printStackTrace();
+				System.exit(1);
+			}
+		}
 	}
 
 	/**
